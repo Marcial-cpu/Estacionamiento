42f177fc3ce286a588e722b54b7a18b6ad88e461
Marcial-cpu <marcial.polanco99@gmail.com>
2020-10-08 17:57:40 -0300

    descripción

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -7,7 +7,7 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    private Button btnir;
+    private Button btnir,btnir1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
