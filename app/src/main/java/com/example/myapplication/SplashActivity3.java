c98106d61859fbd82e1bf79dc1dd8c297e8b9585
Marcial-cpu <marcial.polanco99@gmail.com>
2020-11-07 16:47:03 -0300

    descripción

---
+++ app/src/main/java/com/example/myapplication/SplashActivity3.java
@@ -0,0 +1,25 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash3);
+
+        new Handler () .postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity3.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },2000);
+    }
+}
